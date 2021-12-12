230d783000c853f6aede4b33ec53c3517c01b809
David Nguyen <davebox309@gmail.com>
2021-12-12 14:36:29 -0500

    post method is there , what does it do?

--- Client/src/main/java/controllers/IdController.java
+++ Client/src/main/java/controllers/IdController.java
@@ -40,32 +40,35 @@ public class IdController {
     }
 
     public Id postId(Id id) {
-//        HttpClient httpClient = (HttpClient) HttpClient.newBuilder();
-//        String json = new StringBuilder()
-//                .append("{")
-//                .append("\"name\":\"mkyong\",")
-//                .append("\"notes\":\"hello\"")
-//                .append("}").toString();
-//
-//        try {
-//            HttpRequest request = HttpRequest.newBuilder()
-//                    .POST(HttpRequest.BodyPublishers.ofString(json))
-//                    .uri(URI.create("https://httpbin.org/post"))
-//                    .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
-//                    .header("Content-Type", "application/json")
-//                    .build();
-//
-//            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//            List<Id> idList = objectMapper.readValue(response.body(), new TypeReference<List<Id>>() {
-//            });
-//
-//        }
-//        catch(Exception e) {
-//
-//            // create json from id
-//            // call server, get json result Or error
-//            // result json to Id obj
-//        }
+
+        String json = new StringBuilder()
+                .append("{")
+                .append("\"uid\":\"uid\",")
+                .append("\"name\":\"\"name")
+                .append("\"github\":\"github\"")
+                .append("}").toString();
+
+        try {
+            HttpClient httpClient = (HttpClient) HttpClient.newBuilder();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .uri(URI.create("https://zipcode.rocks:8085/ids"))
+                    .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            List<Id> idList = objectMapper.readValue(response.body(), new TypeReference<List<Id>>() {
+            });
+            System.out.println(response.body());
+
+        }
+        catch(Exception e) {
+
+            // create json from id
+            // call server, get json result Or error
+            // result json to Id obj
+        }
         return null;
     }
 
